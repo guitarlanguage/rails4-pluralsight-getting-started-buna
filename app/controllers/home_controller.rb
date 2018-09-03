a3dc1106cde6fe8585eccadb3e65dc4f37bce51e
Tom LaChance <guitarlanguageseries@gmail.com>
2018-09-03 11:51:20 -0500

    create question view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,4 +4,12 @@ class HomeController < ApplicationController
   
   def about
   end
+
+  def temp
+    redirect_to root_path
+  end
+
+  def question
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
 
   get '/about' => 'home#about'
 
+  post '/questions' => 'home#temp'
+
+  get '/questions/:id' = 'home#question'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
